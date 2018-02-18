560cf8340cb705e2bc52f47f92d68d592b8d9274
Forrest Baer <gfbaer@gmail.com>
2018-02-18 08:01:46 -0700

    adding offset for colorado in isbefore check, could eventually do this with moment timezone stuff

--- js/corepower.js
+++ js/corepower.js
@@ -5,7 +5,6 @@
     var today = moment().format('YYYY-MM-DD');
 
     function init() {
-        getClasses();
         setWatches();
     }
 
@@ -13,8 +12,7 @@
 
         $(document).keyup(function(e) {
             if (e.keyCode === 27) {
-                showClasses();
-                $('.yogaSelector').toggleClass('visible');
+                getClasses();
             }
         });
     }
@@ -35,9 +33,12 @@
             ];
             Promise.all(requests).then((combined) => {
                 localStorage.setItem(today, JSON.stringify(combined));
+                showClasses();
             }).catch((error) => {
                 console.error(error);
             });
+        } else {
+            showClasses();
         }
     }
 
@@ -61,6 +62,7 @@
     function showClasses() {
         var templateData = getTemplateData();
         renderData(templateData);
+        $('.yogaSelector').toggleClass('visible');
     }
 
     function getTemplateData() {
@@ -86,6 +88,7 @@
                         className: shortName,
                         start: moment.utc(new Date(cN.start_date_time)).format('h:mm a'),
                         start2: moment.utc(new Date(cN.start_date_time)),
+                        start3: cN.start_date_time,
                         teacher: cN.teacher.first_name,
                         teacherPhoto: photo
                     });
@@ -98,13 +101,13 @@
 
     function renderData(tD) {
         var toAppend = "";
-        var now = moment(new Date());
+        var now = moment(new Date()).utc();
 
         tD.forEach(function (studio) {
             toAppend += '<div class="aRow">\n';
             toAppend += ('<h2>' + studio.studioName + '</h2>\n');
             studio.classes.forEach(function (aClass) {
-                var passed = moment(new Date()).isBefore(aClass.start2) ? '' : 'passed';
+                var passed = now.isBefore(aClass.start2.add(7,'h')) ? '' : 'passed';
                 toAppend += '<div class="'+passed+'"><img src="' + aClass.teacherPhoto + '" /><h3>' + aClass.className + ' ' + aClass.teacher + '<br/>' + aClass.start + '</h3></div>\n';
             });
             toAppend += '</div>\n';
